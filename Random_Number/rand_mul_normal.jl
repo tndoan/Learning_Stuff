c5a442aa86a615449b1387498ac18ff27a02f0a4
tndoan <doan.thanh.nam.1988@gmail.com>
2014-06-09 10:37:19 +0800

    add function to check conditions of input

--- Random_Number/rand_mul_normal.jl
+++ Random_Number/rand_mul_normal.jl
@@ -4,6 +4,15 @@ function rand_multi_normal(number, mean, stdev)
     # stdev: standard deviation (covariation matrix)
     # return matrix whose size is n x number
     N = size(mean, 1)
+
+    if ndims(stdev) != 2 # check the dimension of covariance matrix
+        error("size of covariance matrix must be 2")
+    end
+    if size(stdev, 1) == size(stdev, 2) == N
+        # check the size of co-var matrix and mean vector
+        error("size of inputs is inconsistant")
+    end
+
     result = zeros(N, number) # initialize the result
     A = chol(stdev)
     for i = 1:number
