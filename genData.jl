2e210e6230a169f04e710883e27166353e8094f1
tndoan <doan.thanh.nam.1988@gmail.com>
2015-07-14 20:28:06 +0800

    commit function to generate data

---
+++ genData.jl
@@ -0,0 +1,11 @@
+
+function generateData(w::Array{Float64, 1}, numPoints::Int64)
+    # w: vector of groundtruth
+    # numPoints: number of data points that we want to generate
+    n = length(w)
+    r = randn(numPoints, n)
+    ra = rand(numPoints, n) * 100.0 - 50.0 # uniform random from -50 to 50
+    x = broadcast(+, r, ra)
+    y = x * w
+    return x, y
+end
