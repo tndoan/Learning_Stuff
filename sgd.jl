8cbb8be6beed24e29e5a146a4ae476f08c1ea301
tndoan <doan.thanh.nam.1988@gmail.com>
2015-07-16 17:05:16 +0800

    add SGD and SVRG algorithm

---
+++ sgd.jl
@@ -0,0 +1,22 @@
+require("derivative.jl")
+
+function sgd(lRate::Float64, data::Array{Float64, 2}, y::Array{Float64, 1})
+    n, m = size(data)
+    w = rand(m) * 100 - 50  # init parameters
+
+    for i=1:n # for each data point
+        point = data[i, :]
+        #println(typeof(point))
+        #println(typeof(w))
+        #println(typeof(y[i]))
+        dev = takeDerivative(w, vec(point), y[i])
+        w = w - lRate * dev
+        
+        # objective function
+        s = data * w - y
+        s = sum(s .* s)
+        println(s) # print the minimum value
+    end
+
+    println(w) # print learned parameters
+end
