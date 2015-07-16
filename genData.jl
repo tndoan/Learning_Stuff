2b824c8588a84f461fd43e0437e8d7aac7bf9d9c
tndoan <doan.thanh.nam.1988@gmail.com>
2015-07-16 17:04:48 +0800

    fix bug

--- genData.jl
+++ genData.jl
@@ -5,7 +5,7 @@ function generateData(w::Array{Float64, 1}, numPoints::Int64)
     n = length(w)
     r = randn(numPoints, n)
     ra = rand(numPoints, n) * 100.0 - 50.0 # uniform random from -50 to 50
-    x = broadcast(+, r, ra)
+    x = r + ra
     y = x * w
     return x, y
 end
